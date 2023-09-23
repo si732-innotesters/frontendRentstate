0b6e4ce46109b7782e2c8bca87f6015c8edad02d
Leydi-Aro <89089568+Leydi-Aro@users.noreply.github.com>
2023-09-23 18:19:13 -0500

    forum-list and forum-see
    feat: forum-list y forum-see

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,34 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RegisterComponent } from './components/register/register.component';
+import {ForumListComponent} from "./components/forum-list/forum-list.component";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatButtonModule} from "@angular/material/button";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {ForumSeeComponent} from "./components/forum-see/forum-see.component";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    RegisterComponent
+    RegisterComponent,
+    ForumListComponent,
+    ForumSeeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/forum-see/forum-see.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-see',
+  templateUrl: './forum-see.component.html',
+  styleUrls: ['./forum-see.component.css']
+})
+export class ForumSeeComponent {
+
+}
