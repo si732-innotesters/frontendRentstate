8f0e6ddbed9735ce3618e7729b5ac57554aa9a46
GrandProgTest <u202124343@upc.edu.pe>
2024-08-19 15:13:20 -0500

    feat: small changes

--- src/app/envieonments/environment.ts
+++ src/app/envieonments/environment.ts
@@ -1,8 +1,5 @@
 
 export const environment= {
 
-  //serverBasePath: 'http://localhost:8080',
-  //serverBasePath: 'http://rentstate.sa-east-1.elasticbeanstalk.com',
-  serverBasePath: 'https://full-rentstate.azurewebsites.net'
-
+  serverBasePath: 'http://localhost:8080',
 }
